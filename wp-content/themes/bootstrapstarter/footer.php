7de33616d93ae40c8b698d4f613fd63d37f31d70
sulemigoreng <sulemigoreng@gmail.com>
2019-09-24 20:53:24 +0700

    Footer and style for footer

--- wp-content/themes/bootstrapstarter/footer.php
+++ wp-content/themes/bootstrapstarter/footer.php
@@ -2,10 +2,38 @@
 
 </div><!-- /.container -->
 
-<footer class="blog-footer">
-    <p>Blog template built for <a href="http://getbootstrap.com">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
-    <p><a href="#">Back to top</a></p>
+<div class="container-fluid foot1">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                ACESSO À INFORMAÇÃO
+            </div>
+            <div class="col-md-4">
+                ÁREA DE IMPRENSA
+            </div>
+            <div class="col-md-4">
+                IDENTIDADE VISUAL DA MARCA
+            </div>
+        </div>
+    </div>
+</div>
+<footer class="container-fluid foot2">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                addslashes
+            </div>
+            <div class="col-md-4">
+                asdasdas
+            </div>
+            <div class="col-md-4">
+                asdasd
+            </div>
+        </div>
+    </div>
 </footer>
+
 <?php wp_footer(); ?>
 </body>
+
 </html>
