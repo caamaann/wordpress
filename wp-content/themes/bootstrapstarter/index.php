7fe84a361d2a54096b611ec732669d3923f98de2
melinanurliana <36650765+melinanurliana@users.noreply.github.com>
2019-09-27 09:18:50 +0700

    fixfinallagimel

--- wp-content/themes/bootstrapstarter/index.php
+++ wp-content/themes/bootstrapstarter/index.php
@@ -410,7 +410,7 @@
             <div class="row">
                 <div class="col-md-4"></div>
                 <div class="col-md-4"></div>
-                <div class="col-md-4 offset-md-8">
+                <div class="col-md-4 jaraklink">
                     <a class="link-more" href="#"><u>Veja mais vídeos</u></a>
                 </div>
             </div>
